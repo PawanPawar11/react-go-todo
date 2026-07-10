5212e9b622cf2edb154fcb876440a4a1f71fbce5
PawanPawar11 <pawanpawar4369@gmail.com>
2026-07-10 11:46:04 +0530

    feat: fetch all todos using /api/todos

--- main.go
+++ main.go
@@ -1,7 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"os"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
+)
+
+type Todo struct {
+	ID        int    `json:"id"`
+	Body      string `json:"body"`
+	Completed bool   `json:"completed"`
+}
 
 func main() {
-	fmt.Println("Hello World!")
+	if os.Getenv("ENV") != "production" {
+		if err := godotenv.Load(".env"); err != nil {
+			log.Fatal("Error while loading .env file:", err.Error())
+		}
+	}
+
+	// Sample Block
+	/*
+		todos := []Todo{
+			{ID: 1, Body: "Sunday", Completed: true},
+			{ID: 2, Body: "Monday", Completed: false},
+			{ID: 3, Body: "Tuesday", Completed: true},
+		}
+	*/
+
+	todos := []Todo{}
+
+	app := fiber.New()
+
+	app.Get("/api/todos", func(c *fiber.Ctx) error {
+		return c.Status(200).JSON(todos)
+	})
+
+	app.Listen("127.0.0.1:8080")
 }
