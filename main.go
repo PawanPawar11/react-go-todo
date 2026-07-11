53875ffbe5b5e0865ba6934507f556fe49afa678
PawanPawar11 <pawanpawar4369@gmail.com>
2026-07-11 17:50:42 +0530

    feat: update server port configuration to use environment variable

--- main.go
+++ main.go
@@ -28,5 +28,10 @@ func main() {
 
 	routes.SetupTodoRoutes(app)
 
-	log.Fatal(app.Listen(":8080"))
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	log.Fatal(app.Listen(":" + port))
 }
